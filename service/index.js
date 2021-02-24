23b92a7f65f9cf3013ceb5c86edef0c7e81ba1da
Bernard Adhitya <bernard.adhitya@gmail.com>
2021-02-24 20:58:04 +0700

    [Bernard] Add spotify web API

--- service/index.js
+++ service/index.js
@@ -1,4 +1,12 @@
+import scopes from './scopes';
+let SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express')
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: '505bd22ab9a141458e7b7286aa184061',
+  clientSecret: 'aad8b58f03ef4ca1bfd7318f07bbbd64',
+  redirectUri: 'http://localhost:8888/callback'
+});
 
 const app = express();
 
